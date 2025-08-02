d0db03d7713231e031e7e6d0dbf21d1360a86e73
LePuncake321 <lucky713ph@yahoo.com>
2025-08-02 16:52:54 -0700

    Window Manager now a prefab, and window creation now streamlined

--- Looper/Assets/Scripts/Window/CreateWindow.cs
+++ Looper/Assets/Scripts/Window/CreateWindow.cs
@@ -8,19 +8,18 @@ public class CreateWindow : MonoBehaviour
     [SerializeField]
     private GameObject prefabToSpawn;
     public Transform spawnPoint;
-
     public GameObject mailParent;
+
+    public void OnAwake()
+    {
+        prefabToSpawn = Resources.Load<GameObject>("Prefabs/ProtoWindow");
+    }
 
     public void makeWindow()
     {
         if (spawnPoint != null)
         {
-            GameObject newWindow = Instantiate(prefabToSpawn);
-
-            if (mailParent != null)
-            {
-                newWindow.transform.SetParent(mailParent.transform);
-            }
+            GameObject newWindow = Instantiate(prefabToSpawn, spawnPoint.transform.position, spawnPoint.transform.rotation,mailParent.transform);
         }
         else
         {
