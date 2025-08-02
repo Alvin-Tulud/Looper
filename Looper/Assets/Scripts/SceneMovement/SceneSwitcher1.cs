9fe24c8917276dfade37a866b6f5de1c5594ae93
Shaheel <ninteskulldo@gmail.com>
2025-08-02 15:31:59 -0700

    fixed fonts and added the login screen
    login screen can be clicked to take us to main game screen

---
+++ Looper/Assets/Scripts/SceneMovement/SceneSwitcher1.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneSwitcher1 : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void goToGame(int world)
+    {
+        //This method will switch the current scene to the game screen.
+        SceneManager.LoadScene(world);
+    }
+
+}
