c59c13103bfa72785f8b11efad5a6d6d0ab1605a
LePuncake321 <lucky713ph@yahoo.com>
2025-08-01 19:32:56 -0700

    Created a new environment to create the window for making emails, and made them draggable

---
+++ Looper/Assets/Scripts/Window/DragWindow.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragWindow : MonoBehaviour
+{
+    [SerializeField]
+    private Canvas canvas;
+
+    public void DragHandler(BaseEventData data)
+    {
+        PointerEventData pointerData = (PointerEventData) data;
+
+        Vector2 pos;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            (RectTransform)canvas.transform,
+            pointerData.position,
+            canvas.worldCamera,
+            out pos);
+
+        transform.position = canvas.transform.TransformPoint(pos);
+    }
+}
