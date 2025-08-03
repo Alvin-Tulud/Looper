62fd58a7769f24c113caeb773fbb0eeed07bc70e
mm8wood <tw802326@gmail.com>
2025-08-03 07:20:51 -0400

    window click sfx

--- Looper/Assets/Scripts/Window/WindowOperation.cs
+++ Looper/Assets/Scripts/Window/WindowOperation.cs
@@ -9,15 +9,18 @@ public class WindowOperation : MonoBehaviour
     public void minimize()
     {
         window.SetActive(false);
+        AudioSO.PlayOneShot("event:/windowClick");
     }
 
     public void maximize()
     {
         window.SetActive(true);
+        AudioSO.PlayOneShot("event:/windowClick");
     }
 
     public void close()
     {
+        AudioSO.PlayOneShot("event:/windowClick");
         Destroy(window);
         Destroy(icon);
         Destroy(entireObject);
